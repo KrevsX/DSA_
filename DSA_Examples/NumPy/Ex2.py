1bec79678d0527f85acbf144c6c8e7503882538d
Kevin Alas <elmrochino.yo@gmail.com>
2023-03-31 23:34:37 -0600

    Amend.//

---
+++ DSA_Examples/NumPy/Ex2.py
@@ -0,0 +1,4 @@
+import numpy as np
+print("Example 1")
+np.array([1,2,3,4,5])
+print(np.array([1,2,3,4,5]))
---
+++ DSA_Examples/PR_1/Ex1.py
@@ -0,0 +1,12 @@
+import math
+from math import sqrt,sin
+
+
+print("Example 1")
+A=16
+print(math.sqrt(A))
+print("Example 2 BD")
+A=16
+B=3.14
+print(sqrt(A))
+print(sin(B))
